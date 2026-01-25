9cbb189a0b47e3176dd83de98e88ef73151347c6
arcobaleno <jymark.borja@gmail.com>
2026-01-25 15:01:04 +0800

    add: new user form alert

--- src/app/FormUI.tsx
+++ src/app/FormUI.tsx
@@ -12,15 +12,35 @@ import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
 import { Axe, Bot } from "lucide-react";
 import React, { useState } from "react";
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Alert, AlertTitle } from "@/components/ui/alert";
+import { FormState } from "@/lib/utils";
 
 export default function FormUI() {
   const [formData, setFormData] = useState({
     username: "",
-    eventCode:""
+    eventCode: "",
   });
+
+  const [formState, setFormState] = useState<FormState>({
+    isSuccess: false,
+    isSubmit: null,
+  });
+
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
+
+    setFormState({
+      isSuccess: false,
+      isSubmit: null,
+    });
+
     try {
       const response = await fetch("/api/loginAndAuthorize", {
         method: "POST",
@@ -33,8 +53,10 @@ export default function FormUI() {
         throw new Error(data.message || "Login failed");
       }
 
-      console.log("Login success:", data);
-
+      setFormState({
+        isSuccess: data.success,
+        isSubmit: true,
+      });
     } catch (error) {
       console.log(error);
     }
@@ -43,19 +65,20 @@ export default function FormUI() {
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
     console.log(name, value);
-    
+
     setFormData((prev) => ({
       ...prev,
       [name]: value,
     }));
   };
 
-  const handleSelectChange = (name: keyof typeof formData) => (value: string) => {
-    setFormData((prev) => ({
-      ...prev,
-      [name]: value,
-    }));
-  };
+  const handleSelectChange =
+    (name: keyof typeof formData) => (value: string) => {
+      setFormData((prev) => ({
+        ...prev,
+        [name]: value,
+      }));
+    };
 
   return (
     <Card className="max-w-md w-full p-8">
@@ -66,6 +89,24 @@ export default function FormUI() {
         </CardTitle>
       </CardHeader>
       <CardContent>
+        {formData.username.length > 0 && formState.isSubmit ? (
+          <Alert
+            className={`mb-4 ${
+              formState.isSuccess
+                ? "border-teal-500 text-teal-900"
+                : "border-red-400 bg-red-50"
+            }`}
+          >
+            <AlertTitle>
+              {formState.isSuccess
+                ? `Success: ${formData.username} ✅`
+                : `Failed: ${formData.username} ❌`}
+            </AlertTitle>
+          </Alert>
+        ) : (
+          ""
+        )}
+
         <Select name="username" onValueChange={handleSelectChange("username")}>
           <SelectTrigger className=" ">
             <SelectValue placeholder="Username" />
